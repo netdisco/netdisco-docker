09318368bd207acb73c1b9cda2ac170f31f70d16
Les Begnaud <lbegnaud@radersolutions.com>
2017-10-10 12:40:07 -0500

    a

--- backup.sh
+++ backup.sh
@@ -3,7 +3,12 @@
 ## pulled from: https://metacpan.org/pod/distribution/App-Netdisco/lib/App/Netdisco/Manual/Deployment.pod#Database-Backups
 ## modified to pull host and creds from deployment.yml
 
+prefix=$1
 
+if [ "$prefix" == "" ]
+then
+    prefix="cron"
+fi
 
 backup_dir="${NETDISCO_HOME}/backups"
 
@@ -22,16 +27,17 @@ NETDISCO_DB_PORT=${NETDISCO_DB_PORT:="5432"}
 
 if [ ! -e "~/.pgpass" ]; then
 
-    cat << EOF > "~/.pgpass" 
+    cat << EOF > ~/.pgpass 
 ${NETDISCO_DB_HOST}:${NETDISCO_DB_PORT}:netdisco:${NETDISCO_DB_USER}:${NETDISCO_DB_PASS}
 EOF
-    chmod 600 "~/.pgpass" 
+    chmod 600 ~/.pgpass 
 fi
 
 mkdir -p $backup_dir
 
 
-DATE=`date +%Y%m%d`
-/usr/bin/pg_dump -F c --create -f $backup_dir/netdisco-pgsql-$DATE.dump --host=${NETDISCO_DB_HOST} --port=${NETDISCO_DB_PORT} --user=${NETDISCO_DB_USER} netdisco
-gzip -9f /path/to/backups/netdisco-pgsql-$DATE.dump
+DATE=`date +%Y%m%d_%H%M%S`
+backup_file=$backup_dir/$prefix-netdisco-pgsql-$DATE.dump
+/usr/bin/pg_dump -F c --create -f $backup_file --host=${NETDISCO_DB_HOST} --port=${NETDISCO_DB_PORT} --user=${NETDISCO_DB_USER} netdisco
+gzip -9f $backup_file
 /usr/bin/find $backup_dir -type f -ctime +30 -exec rm {} \;
