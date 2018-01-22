c4d248b29759948f48da1f89cb54186240893fa7
Oliver Gorwits <oliver@cpan.org>
2018-01-22 11:26:29 +0000

    fix createuser for pg

--- netdisco-postgresql/netdisco-initdb.sh
+++ netdisco-postgresql/netdisco-initdb.sh
@@ -3,16 +3,13 @@
 NETDISCO_DB_NAME="${NETDISCO_DB_NAME:-netdisco}"
 NETDISCO_DB_USER="${NETDISCO_DB_USER:-netdisco}"
 NETDISCO_DB_PASS="${NETDISCO_DB_PASS:-netdisco}"
-NETDISCO_ADMIN_USER="${NETDISCO_ADMIN_USER:-guest}"
 
+PGUSER="${PGUSER:-postgres}"
 psql=( psql -X -v ON_ERROR_STOP=0 -v ON_ERROR_ROLLBACK=on )
-psql+=( --username ${NETDISCO_DB_USER} --dbname ${NETDISCO_DB_NAME} )
+psql+=( --username ${PGUSER} --dbname template1 )
 
-echo >&2 "netdisco-db-entrypoint: configuring Netdisco db user"
-echo "*:*:${NETDISCO_DB_NAME}:${NETDISCO_DB_USER}:${NETDISCO_DB_PASS}" > ~/.pgpass
-chmod 0600 ~/.pgpass
-createuser -DRSw ${NETDISCO_DB_USER}
+echo >&2 "netdisco-db-entrypoint: configuring Netdisco user and db"
+"${psql[@]}" -c "CREATE ROLE ${NETDISCO_DB_USER} WITH NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD '${NETDISCO_DB_PASS}'"
 createdb -O ${NETDISCO_DB_USER} ${NETDISCO_DB_NAME}
-rm ~/.pgpass
 
 /usr/local/bin/netdisco-updatedb.sh
