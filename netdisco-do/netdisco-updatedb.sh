aff214f229b61e64d4e8035f677156c3f1371ed4
Oliver Gorwits <oliver@cpan.org>
2025-10-24 11:13:58 +0100

    can now docker-compose build and run with own Pg init and (re-)deploy

--- netdisco-do/netdisco-updatedb.sh
+++ netdisco-do/netdisco-updatedb.sh
@@ -1,37 +1,48 @@
 #!/usr/bin/env bash
+set -uxo pipefail
 
-NETDISCO_ADMIN_USER="${NETDISCO_ADMIN_USER:-guest}"
+# User needs to either set these in docker-compose environment
+# or else run wrapped in netdisco-env
+
+: "${PGDATABASE:=${NETDISCO_DB_NAME:=netdisco}}"
+: "${PGHOST:=${NETDISCO_DB_HOST:=netdisco-postgresql}}"
+: "${PGPORT:=${NETDISCO_DB_PORT:=5432}}"
+: "${PGUSER:=${NETDISCO_DB_USER:=netdisco}}"
+: "${PGPASSWORD:=${NETDISCO_DB_PASS:=netdisco}}"
+
+# the no_auth=true initial login user
+: "${NETDISCO_ADMIN_USER:=guest}"
 
 export COL='\033[0;35m'
 export NC='\033[0m'
 
 psql=( psql -X -v ON_ERROR_STOP=0 -v ON_ERROR_ROLLBACK=on )
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: bringing schema up-to-date${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: bringing schema up-to-date${NC}"
 ls -1 /home/netdisco/perl5/lib/perl5/auto/share/dist/App-Netdisco/schema_versions/App-Netdisco-DB-* | \
   xargs -n1 basename | sort -n -t '-' -k4 | \
   while read file; do
-    "${psql[@]}" -f "/home/netdisco/perl5/lib/perl5/auto/share/dist/App-Netdisco/schema_versions/$file" >/dev/null 2>&1
+    "${psql[@]}" -f "/home/netdisco/perl5/lib/perl5/auto/share/dist/App-Netdisco/schema_versions/$file"
   done
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: importing OUI${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: importing OUI${NC}"
 "${psql[@]}" -f /var/lib/postgresql/netdisco-sql/netdisco-lookup-tables.sql
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: marking schema as up-to-date${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: marking schema as up-to-date${NC}"
 MAXSCHEMA=$(grep VERSION /home/netdisco/perl5/lib/perl5/App/Netdisco/DB.pm | sed 's/[^0-9]//g')
 STAMP=$(date '+v%Y%m%d_%H%M%S.000')
-"${psql[@]}" -c "CREATE TABLE dbix_class_schema_versions (version varchar(10) PRIMARY KEY, installed varchar(20) NOT NULL)" >/dev/null 2>&1
-"${psql[@]}" -c "INSERT INTO dbix_class_schema_versions VALUES ('${MAXSCHEMA}', '${STAMP}')" >/dev/null 2>&1
+"${psql[@]}" -c "CREATE TABLE dbix_class_schema_versions (version varchar(10) PRIMARY KEY, installed varchar(20) NOT NULL)"
+"${psql[@]}" -c "INSERT INTO dbix_class_schema_versions VALUES ('${MAXSCHEMA}', '${STAMP}')"
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: adding admin user if none exists${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: adding admin user if none exists${NC}"
 if [ -z $("${psql[@]}" -A -t -c "SELECT 1 FROM users WHERE admin") ]; then
   "${psql[@]}" -c "INSERT INTO users (username, port_control, admin) VALUES ('${NETDISCO_ADMIN_USER}', true, true)"
 fi
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: adding session key if none exists${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: adding session key if none exists${NC}"
 if [ -z $("${psql[@]}" -A -t -c "SELECT 1 FROM sessions WHERE id = 'dancer_session_cookie_key'") ]; then
   "${psql[@]}" -c "INSERT INTO sessions (id, a_session) VALUES ('dancer_session_cookie_key', md5(random()::text))"
 fi
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: queueing stats job${NC}"
+echo >&2 -e "${COL}netdisco-updatedb: queueing stats job${NC}"
 "${psql[@]}" -c "INSERT INTO admin (action, status) VALUES ('stats', 'queued')"
--- netdisco-postgresql/netdisco-initdb.sh
+++ netdisco-postgresql/netdisco-initdb.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
+set -efuxo pipefail
 
-NETDISCO_DB_NAME="${NETDISCO_DB_NAME:-netdisco}"
-NETDISCO_DB_USER="${NETDISCO_DB_USER:-netdisco}"
-NETDISCO_DB_PASS="${NETDISCO_DB_PASS:-netdisco}"
+# If running in docker-compose, user gets an initialised database
+# using default username/etc as below.
+
+: "${NETDISCO_DB_NAME:=netdisco}"
+: "${NETDISCO_DB_USER:=netdisco}"
+: "${NETDISCO_DB_PASS:=netdisco}"
+
+# Explicit assign; we don't want to pick up PGUSER from netdisco-env
+# (because that will be NETDISCO_DB_USER).
+PGUSER="${NETDISCO_DB_ADMIN_USER:=postgres}"
 
 export COL='\033[0;35m'
 export NC='\033[0m'
 
-PGUSER="${PGUSER:-postgres}"
 psql=( psql -X -v ON_ERROR_STOP=0 -v ON_ERROR_ROLLBACK=on )
-psql+=( --username=${PGUSER} )
 
-echo >&2 -e "${COL}netdisco-db-entrypoint: configuring Netdisco user and db${NC}"
+echo >&2 -e "${COL}netdisco-initdb: configuring Netdisco user and db${NC}"
 "${psql[@]}" -c "CREATE ROLE ${NETDISCO_DB_USER} WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD '${NETDISCO_DB_PASS}'"
 createdb --username=${PGUSER} -O ${NETDISCO_DB_USER} ${NETDISCO_DB_NAME}
