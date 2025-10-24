986f4a6abec43077b5a1035f69f793a98642e876
Oliver Gorwits <oliver@cpan.org>
2025-10-24 14:38:05 +0100

    factor out environment variables

--- netdisco-postgresql/netdisco-initdb.sh
+++ netdisco-postgresql/netdisco-initdb.sh
@@ -8,9 +8,9 @@ set -efuxo pipefail
 : "${NETDISCO_DB_USER:=netdisco}"
 : "${NETDISCO_DB_PASS:=netdisco}"
 
-# Explicit assign; we don't want to pick up PGUSER from netdisco-env
-# (because that will be NETDISCO_DB_USER).
-PGUSER="${NETDISCO_DB_ADMIN_USER:=postgres}"
+# Explicit assign; we don't want to pick up PGUSER from environment
+# because that many be the same as NETDISCO_DB_USER.
+PGUSER="${NETDISCO_DB_SUPERUSER:=postgres}"
 
 export COL='\033[0;35m'
 export NC='\033[0m'
