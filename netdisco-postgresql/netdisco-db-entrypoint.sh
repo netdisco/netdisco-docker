c5c923a553f9a646aea97684a3e9627a500cee06
Oliver Gorwits <oliver@cpan.org>
2025-10-26 21:32:34 +0000

    databases now run and wait for upgrade

--- netdisco-postgresql/netdisco-db-entrypoint.sh
+++ netdisco-postgresql/netdisco-db-entrypoint.sh
@@ -7,23 +7,25 @@ export NC='\033[0m'
 # Explicit assign; we don't want to pick up PGUSER from environment
 # because that many be the same as NETDISCO_DB_USER.
 PGUSER="${NETDISCO_DB_SUPERUSER:=postgres}"
-PGDATABASE=template1
-
 su=( su-exec "${PGUSER}" )
-psql=( psql -X -v ON_ERROR_STOP=0 -v ON_ERROR_ROLLBACK=on )
+
+# fast quit if we've been upgraded already
+if [ -f "${PGDATA}/NETDISCO_UPGRADED" ]; then exit 0; fi
 
 # pass through if we're the current latest version
 if [ "$PG_MAJOR" = "$NETDISCO_CURRENT_PG_VERSION" ]
 then
-  echo >&2 -e "${COL}netdisco-db-entrypoint: starting latest pg${NC}"
+  echo >&2 -e "${COL}netdisco-db-entrypoint: starting latest pg version ${PG_MAJOR}${NC}"
   if [ ! -s "${PGDATA}/PG_VERSION" ]
   then
+      echo >&2 -e "${COL}netdisco-db-entrypoint: generating random POSTGRES_PASSWORD${NC}"
       export POSTGRES_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   fi
+  echo >&2 -e "${COL}netdisco-db-entrypoint: passing control to postgresql docker-entrypoint${NC}"
   exec /usr/local/bin/docker-entrypoint.sh "$@"
 fi
 
-# quit if we were never used and have no data
+# fast quit if we were never used and have no data
 if [ ! -s "${PGDATA}/PG_VERSION" ]; then exit 0; fi
 
 # so we have a database, but we are not the current.
@@ -34,7 +36,7 @@ TEST_TO=$(($NETDISCO_CURRENT_PG_VERSION - 1))
 
 if [$TEST_FROM -le $TEST_TO]
 then
-  for VER in $(seq $TEST_FROM $TEST_TO )
+  for VER in $(seq $TEST_FROM $TEST_TO)
   do
     if [ -s "/var/lib/postgresql/$VER/docker/PG_VERSION" ]
     then
@@ -44,16 +46,18 @@ then
 fi
 
 # otherwise run on special port for data migration, wait until data deployed, then quit
+echo >&2 -e "${COL}netdisco-db-entrypoint: ready for upgrade; starting on port 50432${NC}"
 
 NOTIFY_SOCKET= "${su[@]}" \
   pg_ctl -D "$PGDATA" \
   -o "-c listen_addresses='*' -p 50432 -c log_min_error_statement=LOG -c log_min_messages=LOG" \
   -w start
 
-while [ -z $("${psql[@]}" -h "${NETDISCO_DB_HOST}" -A -t -c "SELECT 1 FROM sessions WHERE id = 'dancer_session_cookie_key'") ]
+while [ ! -f "${PGDATA}/NETDISCO_UPGRADED" ]
 do
    sleep 2
 done
+echo >&2 -e "${COL}netdisco-db-entrypoint: data migration complete; stopping${NC}"
 
 sleep 2
 "${su[@]}" pg_ctl -D "$PGDATA" -m fast -w stop
